e3489153475215f132eae2e1a04b33d28ce83a13
Connor Kennelly <knelly26@gmail.com>
2016-07-29 21:36:37 -0700

    Clean sentinel value and handle edge cases in Player.IsActive()

--- MoochKick/MoochKick/Player.cs
+++ MoochKick/MoochKick/Player.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace MoochKick
 {
     /// <summary>
-    /// Represents a Halo 5 player.
+    /// Represents a Halo 5 player with gamertag and a set of recent .
     /// </summary>
     class Player
     {
@@ -20,31 +20,32 @@ namespace MoochKick
         public Player(string username)
         {
             gamertag = username;
-            lastActiveDate = DateTime.MinValue;     //TODO: This is set to Jan 1st 1901 right now.
             recentGameDates = new Stack<DateTime>(25);
         }
        
-        /*
-        if the latest match was within the threshold, the player is active.
-        if object has not been initialized, will return false
-        */
-        public bool isActive(int daysInactivityThreshold, int minimumNumberofGames)
+        /// <summary>
+        /// Returns true if player has played y games in x days.  Defaults to 3 games in 14 days.
+        /// </summary>
+        /// <param name="daysInactivityThreshold"></param>
+        /// <param name="minimumNumberofGames"></param>
+        /// <returns></returns>
+        public bool isActive(int daysInactivityThreshold = 14, int minimumNumberofGames = 3)
         {
-            DateTime temp = DateTime.MinValue;
+            DateTime temp = DateTime.UtcNow; //UtcNow handles minimumNumberofGames <= 1
 
             int playersTotalGames = recentGameDates.Count;
-            //can we get rid of sentinel value?
-            // what happens when there are less than 25 recent games?
-            //what happens if min number of game is passed?
+
+            if(playersTotalGames <1)
+            {
+                return false;
+            }
 
             for(int i = 0; i < minimumNumberofGames && i < playersTotalGames; i++)
             {
                 temp = recentGameDates.Pop();
-                //Console.WriteLine("Player {0} popped game date of {1}", gamertag, temp.ToShortDateString());
             }
 
             TimeSpan difference = DateTime.UtcNow - temp;
-            //Console.WriteLine("Comparing game date {0} for {1}", temp.ToShortDateString(), gamertag);
 
             if(difference.Days < daysInactivityThreshold)       
             {
